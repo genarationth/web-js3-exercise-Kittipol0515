75b510f6335bb3214afc18058849a4080c4c7750
kittipol <kittipol.surapinchai@gmail.com>
2023-03-14 14:41:45 +0700

    homework

---
+++ script.js
@@ -0,0 +1,33 @@
+// Exercise 1
+
+
+
+
+// Exercise 2
+// part 1
+
+const name1 = 'Add';
+const name2 = 'sub';
+const name3 = 'divide';
+console.log(`welcome ${name1} ${name2} ${name3}`);
+
+// part 2
+function calculateAge(birthYear) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+    return age;
+  }
+const birthYear = 1990;
+const age = calculateAge(birthYear);
+console.log(age);
+
+
+
+
+
+
+// part 3
+function greeting(name1 , age1 , name2 , age2 , name3 , age3 ) {
+    console.log('Welcome' + name1 +  ", you are" + age1 + ".Welcome" + name2 + ", you are" + age2 + ".Welcome" + name3 + ", you are" + age3);
+}
+greeting("Kitti" , 25 , "hosing" , 27 , "goodie" , 28);
